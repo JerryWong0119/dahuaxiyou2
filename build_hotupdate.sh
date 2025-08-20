8e4aef14354fe3ee64b3418038f274341c431d74
JerryWong <99593311@qq.com>
2025-08-20 20:00:40 +0800

    mac hotupdate shell

---
+++ build_hotupdate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "-"
+echo "- build hotupdate"
+echo 按任意键继续
+read -n 1
+echo "- $1"
+LUAFILES=`find ./template -name "*.*" -type f`
+for file in $LUAFILES
+do
+    FILEURL="scripts/"${file:11}
+    MD5STRING=`md5 $file | awk '{print $4}'`
+    FILESIZE=`wc -c < $file`
+    printf -v RESULT "%s %s %s 1" "$FILEURL" "$MD5STRING" "$FILESIZE"
+    echo $RESULT >> "files_$1.txt"
+done
