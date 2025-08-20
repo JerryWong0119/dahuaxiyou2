f23c7dc7c6a36821408a4e014f6785d14af394f6
JerryWong <99593311@qq.com>
2025-08-20 20:17:28 +0800

    ok

--- build_hotupdate.sh
+++ build_hotupdate.sh
@@ -4,10 +4,10 @@ echo "- build hotupdate"
 echo 按任意键继续
 read -n 1
 echo "- $1"
-LUAFILES=`find ./template -name "*.*" -type f`
+LUAFILES=`find ./hotupdate/scripts -name "*.*" -type f`
 for file in $LUAFILES
 do
-    FILEURL="scripts/"${file:11}
+    FILEURL="scripts/"${file:20}
     MD5STRING=`md5 $file | awk '{print $4}'`
     FILESIZE=`wc -c < $file`
     printf -v RESULT "%s %s %s 1" "$FILEURL" "$MD5STRING" "$FILESIZE"
